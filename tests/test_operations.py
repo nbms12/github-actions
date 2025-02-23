0dd12e0f5e86704e2797b7370db8379a28f3a4f1
WINDOWSPRO10\manju <manjunath.blyr@gmail.com>
2025-02-23 19:52:28 +0530

    files commit

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.operations import add , sub,mult 
+
+
+def test_add_operation():
+    assert add(2,5)==7
+    assert add(2,99)==101
+    assert add(2,0)==2
+
+def test_sub_operations():
+    assert sub(3,3)==0
+
+def test_multi_operations():
+    assert mult(3,9)==27
